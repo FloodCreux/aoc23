4b0cdbb9ba0a110711d32665934e6e416a62ef48
Mike Flood <flood.mike@gmail.com>
2023-12-05 19:00:09 -0600

    feat: cube part 2 - answer too high

--- day2/cube/bin/main.ml
+++ day2/cube/bin/main.ml
@@ -2,6 +2,12 @@ let file_channel = open_in "input.txt"
 let games = Cube.solve_part_1 file_channel;;
 
 print_int games;;
-Printf.printf "\n"
+Printf.printf "\n";;
+close_in file_channel
+
+let file_channel = open_in "input.txt"
+let games_2 = Cube.solve_part_2 file_channel;;
+
+print_int games_2
 
 let () = close_in file_channel
--- day2/cube/lib/cube.ml
+++ day2/cube/lib/cube.ml
@@ -12,6 +12,7 @@ let parse_score score =
   let split_score = String.split_on_char ' ' (String.trim score) in
   (List.nth split_score 1, List.hd split_score)
 
+(* Part 1 Logic *)
 let is_valid_score color score =
   match color with
   | "red" -> int_of_string score <= 12
@@ -71,6 +72,49 @@ let solve_part_1 stdin =
   let all_lines = input_lines stdin in
   let games = List.map parse_game_line all_lines in
   let solved_games = solve_scores games in
+  let rec process_games arr =
+    match arr with [] -> 0 | x :: xs -> x + process_games xs
+  in
+  process_games solved_games
+
+(* Part 2 Logic *)
+let parse_game_rounds_2 rounds =
+  let split_rounds = String.split_on_char ';' rounds in
+  List.fold_left
+    (fun acc round ->
+      StringMap.union
+        (fun _ a b ->
+          let left_score = int_of_string a in
+          let right_score = int_of_string b in
+          if left_score <= right_score then Some a else Some b)
+        acc (parse_round round))
+    StringMap.empty split_rounds
+
+let parse_game_line_2 line =
+  let split_line = String.split_on_char ':' line in
+  let game_id =
+    int_of_string (List.nth (String.split_on_char ' ' (List.hd split_line)) 1)
+  in
+  let minimum_numbers = parse_game_rounds_2 (List.nth split_line 1) in
+  (game_id, minimum_numbers)
+
+let solve_power_scores games =
+  let rec solve games lst =
+    match games with
+    | [] -> lst
+    | (_, game_scores) :: rest ->
+        let red_score = int_of_string (StringMap.find "red" game_scores) in
+        let blue_score = int_of_string (StringMap.find "blue" game_scores) in
+        let green_score = int_of_string (StringMap.find "green" game_scores) in
+        let pow = red_score * blue_score * green_score in
+        solve rest (pow :: lst)
+  in
+  solve games []
+
+let solve_part_2 stdin =
+  let all_lines = input_lines stdin in
+  let games = List.map parse_game_line_2 all_lines in
+  let solved_games = solve_power_scores games in
   let rec process_games arr =
     match arr with [] -> 0 | x :: xs -> x + process_games xs
   in
