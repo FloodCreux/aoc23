d2ca71c69bca0c04026a8b6db054ecc4be650f0e
Mike Flood <flood.mike@gmail.com>
2023-12-08 13:05:35 -0600

    fix(gears): closer....

--- day3/gears/bin/main.ml
+++ day3/gears/bin/main.ml
@@ -1,5 +1,7 @@
-let file_channel = open_in "input.txt";;
+let file_channel = open_in "input.txt"
+let result_1 = Gears.solve_part_1 file_channel;;
 
-Gears.solve_part_1 file_channel
+print_endline "Part 1:";;
+List.iter (fun (x, _, _) -> Printf.printf "%s\n" x) result_1
 
 let () = close_in file_channel
--- day3/gears/lib/gears.ml
+++ day3/gears/lib/gears.ml
@@ -8,7 +8,7 @@ let input_lines stdin =
 
 let is_digit c = Char.code c >= Char.code '0' && Char.code c <= Char.code '9'
 
-let check_digits cur _prev _next =
+let get_digits cur =
   let rec process_curr i digits =
     if i >= String.length cur then digits
     else
@@ -36,21 +36,38 @@ let check_digits cur _prev _next =
   in
   process_curr 0 []
 
+let filter_digits cur line prev next =
+  let check_line line start stop =
+    let right = if start > 0 then String.get line (start - 1) else '.' in
+    let left =
+      if stop < String.length line - 1 then String.get line (stop + 1) else '.'
+    in
+    right != '.' || left != '.'
+  in
+  List.filter
+    (fun (_d, start, stop) ->
+      match (prev, next) with
+      | None, None -> check_line line start stop
+      | Some prev, None ->
+          check_line line start stop || check_line prev start stop
+      | None, Some next ->
+          check_line line start stop || check_line next start stop
+      | Some prev, Some next ->
+          check_line line start stop || check_line prev start stop
+          || check_line next start stop)
+    cur
+
 let solve_part_1 lst =
   let all_lines = input_lines lst in
-  let rec solve = function
-    | [] -> ()
+  let rec solve lst result =
+    match lst with
+    | [] -> result
     | curr :: rest ->
         let prev = if rest = [] then None else Some (List.hd rest) in
         let next =
           if List.length rest > 1 then Some (List.nth rest 1) else None
         in
-        let digits = check_digits curr prev next in
-        List.iter
-          (fun (s, start, stop) ->
-            Printf.printf "%s: start - %d, end - %d\n" s start stop)
-          digits;
-        Printf.printf "---------------------------\n";
-        solve rest
+        let digits = get_digits curr in
+        solve rest (filter_digits digits curr prev next)
   in
-  solve all_lines
+  solve all_lines []
