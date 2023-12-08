a3465c085c07735747706cbb63a60f7bec1bd31e
Mike Flood <flood.mike@gmail.com>
2023-12-08 07:14:28 -0600

    fix(gears): start adding comparison logic

--- day3/gears/lib/gears.ml
+++ day3/gears/lib/gears.ml
@@ -6,60 +6,45 @@ let input_lines stdin =
   in
   input []
 
-let get_rows lst = List.length lst
-let get_cols lst = List.hd lst |> String.length
+let is_digit c = Char.code c >= '0' && Char.code c <= '9'
 
-let create_matrix lst =
-  let rows = get_rows lst in
-  let cols = get_cols lst in
-  let matrix = Array.make_matrix rows cols ' ' in
-  let rec fill_matrix lst row =
-    match lst with
-    | [] -> ()
-    | hd :: tl ->
-        let rec fill_col col =
-          if col < String.length hd then (
-            matrix.(row).(col) <- hd.[col];
-            fill_col (col + 1))
-        in
-        if row < rows then fill_col 0;
-        fill_matrix tl (row + 1)
-  in
-  fill_matrix lst 0;
-  matrix
-
-let check_first_row row next_row =
-  let rec check row next_row col valid =
-    if col >= Array.length row then valid
+let check_digits cur prev next =
+  let rec process_curr i digits =
+    if i >= Array.length cur then digits
     else
-      let item = Array.get row col in
-      if item = '.' then check row next_row (col + 1) valid
-      else
-        let valid =
-          if col > 0 && col < Array.length row then
-            let left = Array.get row (col - 1) in
-            let bottom_left = Array.get next_row (col - 1) in
-            let bottom = Array.get next_row col in
-            let bottom_right = Array.get next_row (col + 1) in
-            valid || left != '.' || bottom_left != '.' || bottom != '.'
-            || bottom_right != '.'
-          else if col = 0 then
-            let bottom_left = Array.get next_row (col - 1) in
-            let bottom = Array.get next_row col in
-            let bottom_right = Array.get next_row (col + 1) in
-            valid || bottom_left != '.' || bottom != '.' || bottom_right != '.'
-          else if col = Array.length row then
-            let left = Array.get row (col - 1) in
-            let bottom_left = Array.get next_row (col - 1) in
-            let bottom = Array.get next_row col in
-            valid || left != '.' || bottom_left != '.' || bottom != '.'
-          else valid
+      let find_next_digit s =
+        let len = String.length s in
+        let rec find_start i =
+          if i >= len then None
+          else if is_digit s.[i] then Some i
+          else find_start (i + 1)
         in
-        check row next_row (col + 1) valid
+        let rec find_end i =
+          if i >= len || not (is_digit s.[i]) then i - 1 else find_end (i + 1)
+        in
+        match find_start i with
+        | Some start_pos ->
+            let end_pos = find_end start_pos in
+            Some (start_pos, end_pos)
+        | None -> None
+      in
+      let curr_digits = process_curr 0 [] in
+      match prev with Some prev -> curr_digits | None -> curr_digits
   in
-  check row next_row 0 false
+  match next with Some next -> curr_digits | None -> curr_digits
 
 let solve_part_1 lst =
-  let matrix = create_matrix lst in
-  let solve row _ = check_first_row row (Array.get matrix 1) in
-  Array.mapi (fun _ row -> solve row 0) matrix
+  let all_lines = input_lines stdin in
+  let solve arr =
+    let len = Array.length arr in
+    let rec process i =
+      if i >= len then ()
+      else
+        let current = arr.(i) in
+        let prev = if i > 0 then Some arr.(i - 1) else None in
+        let next = if i < len - 1 then Some arr.(i + 1) else None in
+        check_digits current prev next
+    in
+    process 0
+  in
+  solve (Array.of_list all_lines)
